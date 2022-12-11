41207b3f05f160d412a85033119f6dc8f0705423
AbhinavChandurkar <abhinavchandurkar55@gmail.com>
2022-12-11 23:22:11 +0530

    p1773

--- Array/p1773.java
+++ Array/p1773.java
@@ -1,12 +1,50 @@
 import java.util.*;
 
 public class p1773 {
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int idx;
+        if(ruleKey.equals("type")){
+            idx = 0;
+        }else if(ruleKey.equals("color")){
+            idx =1;
+        }else{
+            idx=2;
+        }
+        int count = 0;
+        int n = items.size();
+        for (int i = 0; i < n; i++) {
+            // System.out.println(items.get(i).get(idx));
+            if(items.get(i).get(idx) == ruleValue){
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        List<List<Integer>> lists = new ArrayList<>(List.of(
-            new ArrayList<>(List.of(1, 2, 3)),
-            new ArrayList<>(List.of(4, 5, 6)),
-            new ArrayList<>(List.of(6, 7, 8))
-    ));
+        List<String> p1 = new ArrayList<String>();
+        p1.add("phone");
+        p1.add("blue");
+        p1.add("pixel");
+
+        List<String> p2 = new ArrayList<String>();
+        p2.add("computer");
+        p2.add("silver");
+        p2.add("lenovo");
+
+        List<String> p3 = new ArrayList<String>();
+        p3.add("phone");
+        p3.add("gold");
+        p3.add("iphone");
+
+        List<List<String>> items = new ArrayList<>();
+        items.add(p1);
+        items.add(p2);
+        items.add(p3);
+
+        System.out.println(items);
+
+        int ans = countMatches(items,"color","silver");
+        System.out.println(ans);        
 
     }
 }
